f07ab0284198270892995dddf3a004d1690a7e83
huchaojun <huchaojun@izunyou.com>
2020-05-14 09:50:39 +0800

    项目初始化

---
+++ FFPlayer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FFPlayer
+//
+//  Created by saeipi on 2020/5/14.
+//  Copyright © 2020 saeipi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//int main(int argc, const char * argv[]) {
+//    @autoreleasepool {
+//        // insert code here...
+//        NSLog(@"Hello, World!");
+//    }
+//    return 0;
+//}
